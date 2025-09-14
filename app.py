346cdf8f95490bff1a528b207bcde64c77925c97
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-09-13 22:44:08 -0700

    works better now

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import datetime
 import os
 import dotenv
+import schedule
+import time
+import threading
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+
+os.system("prisma db push")
 import prisma
-
-
+db = prisma.Prisma()
+db.connect()
 dotenv.load_dotenv()
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 client = app.client
-db = prisma.Prisma()
-db.connect()
 
 
 MAIN_CHANNEL = os.environ["SLACK_MAIN_CHANNEL"]
@@ -61,5 +65,44 @@ def signout(ack, respond, command):
         respond(":x: You aren't signed in.")
 
 
-client.chat_postMessage(channel=MAIN_CHANNEL, text="coming soon")
+def signout_all_users():
+    users = db.user.find_many(where={
+        "signed_in": True
+    })
+    for user in users:
+        db.user.update(where={
+            "slack_id": user.slack_id
+        }, data={
+            "signed_in": False
+        })
+        client.chat_postMessage(channel=user.slack_id, text=":clown_face: Hi, you forgot to sign out today. Better luck next time!")
+
+
+@app.command("/ss")
+@app.command("/signin-status")
+def signin_status(ack, respond, command):
+    ack()
+    slack_id = command["user_id"]
+    user = db.user.find_first(where={
+        "slack_id": slack_id
+    })
+    if user is None:
+        respond(":bangbang: You've never signed in your life.")
+    elif user.signed_in:
+        respond(f":large_green_square: You've been signed in since {user.signin_time.strftime('%H:%M')}, and have a total of {user.total_hours:.2f} hours.")
+    else:
+        respond(f":large_red_square: You're signed out, and have a total of {user.total_hours:.2f} hours.")
+
+
+schedule.every().day.at("00:00").do(signout_all_users)
+
+
+def schedule_loop():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+schedule_loop_thread = threading.Thread(target=schedule_loop)
+schedule_loop_thread.start()
 SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
