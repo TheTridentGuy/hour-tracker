9235ac8a0582b1592e6845e461e313c58c6a8ace
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-09-13 20:03:45 -0700

    works now

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ MAIN_CHANNEL = os.environ["SLACK_MAIN_CHANNEL"]
 @app.command("/signin")
 def signin(ack, respond, command):
     ack()
-    print(command)
     slack_id = command["user_id"]
     user = db.user.find_first(where={
         "slack_id": slack_id
@@ -30,9 +29,37 @@ def signin(ack, respond, command):
             "slack_id": slack_id
         })
     if not user.signed_in:
-        user.signed_in = True
+        db.user.update(where={
+            "slack_id": slack_id
+        }, data={
+            "signed_in": True,
+            "signin_time": datetime.datetime.now()
+        })
+        respond(":white_check_mark: You've been signed in.")
     else:
-        respond(":x: You're already signed in!")
+        respond(":x: You're already signed in.")
+
+
+@app.command("/so")
+@app.command("/signout")
+def signout(ack, respond, command):
+    ack()
+    slack_id = command["user_id"]
+    user = db.user.find_first(where={
+        "slack_id": slack_id
+    })
+    if user and user.signed_in:
+        new_hours = (datetime.datetime.now() - user.signin_time.replace(tzinfo=None)).seconds / 3600
+        user = db.user.update(where={
+            "slack_id": slack_id
+        }, data={
+            "signed_in": False,
+            "total_hours": user.total_hours + new_hours
+        })
+        respond(f":white_check_mark: You've been signed out. This session you logged {new_hours:.2f} hours, and you now have {user.total_hours:.2f} hours.")
+    else:
+        respond(":x: You aren't signed in.")
+
 
 client.chat_postMessage(channel=MAIN_CHANNEL, text="coming soon")
 SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
